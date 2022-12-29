e9974bec9c24cfac8e18c2e0fea43cd9585969cc
raghav-rama <hackerer528@gmail.com>
2022-12-29 21:14:14 +0530

    feat: add generic input method

---
+++ src/input.rs
@@ -0,0 +1,14 @@
+use std::{fmt::Debug, io, str::FromStr};
+
+pub fn input<T: FromStr>() -> Result<T, <T as FromStr>::Err>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let mut buf = String::new();
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("Problem reading stdin");
+
+    let choice = buf.trim().parse::<T>();
+    choice
+}
