4e0414c2729b11ad0c77062a8655c7b3d9c5ce7a
raghav-rama <hackerer528@gmail.com>
2023-01-04 04:21:05 +0530

    use `default` instead of `new`

--- src/input.rs
+++ src/input.rs
@@ -4,7 +4,7 @@ pub fn input<T: FromStr>() -> Result<T, <T as FromStr>::Err>
 where
     <T as FromStr>::Err: Debug,
 {
-    let mut buf = String::new();
+    let mut buf = String::default();
     io::stdin()
         .read_line(&mut buf)
         .expect("Problem reading stdin");
